08f7fa813b51e41a3932a0cce4c47b57f6fa6a3c
Kiss Adrián Imre <kisswadrian857@gmail.com>
2021-08-01 21:41:14 +0200

    Written functions in BlogPostCRUDMbean class

--- src/main/java/ulyssys/java/course/homework/mbean/BlogPostCRUDMbean.java
+++ src/main/java/ulyssys/java/course/homework/mbean/BlogPostCRUDMbean.java
@@ -1,5 +1,54 @@
 package ulyssys.java.course.homework.mbean;
 
-public class BlogPostCRUDMbean {
+import ulyssys.java.course.homework.entity.BlogPost;
+import ulyssys.java.course.homework.service.AuthorService;
+import ulyssys.java.course.homework.service.BlogPostService;
 
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class BlogPostCRUDMbean implements Serializable {
+
+    private List<BlogPost> list;
+
+    private BlogPost selectedBlogPost;
+
+    @Inject
+    BlogPostService blogPostService;
+
+    @PostConstruct
+    private void init(){
+        list = blogPostService.getAll();
+        selectedBlogPost = new BlogPost();
+    }
+
+    public void save(){
+        //TODO
+    }
+
+    public void remove(){
+        //TODO
+    }
+
+    public List<BlogPost> getList() {
+        return list;
+    }
+
+    public void setList(List<BlogPost> list) {
+        this.list = list;
+    }
+
+    public BlogPost getSelectedBlogPost() {
+        return selectedBlogPost;
+    }
+
+    public void setSelectedBlogPost(BlogPost selectedBlogPost) {
+        this.selectedBlogPost = selectedBlogPost;
+    }
 }
